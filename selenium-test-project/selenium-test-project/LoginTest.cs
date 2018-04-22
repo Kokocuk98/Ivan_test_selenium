b6ba3ca8efe51fbdcf5710a065e3db014fbe586f
Kokocuk98 <kokocuk98@gmail.com>
2018-04-22 08:56:34 +0300

    Сценарий логина/логаута

---
+++ selenium-test-project/selenium-test-project/LoginTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace csharp_example
+{
+    public class LoginTest
+    {
+   
+    public ChromeDriver driver;
+
+        [SetUp]
+        public void Start()
+        {
+        driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void TestLogin()
+        {
+        driver.Url = "http://localhost/litecart/admin";
+        driver.FindElement(By.Name("username")).SendKeys("admin");
+        driver.FindElement(By.Name("password")).SendKeys("admin");
+        driver.FindElement(By.Name("login")).Click();
+        driver.FindElement(By.ClassName("fa-sign-out")).Click();
+        }
+    
+        [TearDown]
+        public void Stop()
+        {
+        driver.Quit();
+        driver = null;
+        }
+    }
+}
+
