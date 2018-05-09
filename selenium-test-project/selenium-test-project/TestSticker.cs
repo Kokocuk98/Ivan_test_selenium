f12dd9ab4e15c05265fe91a52ad377ac38065907
kokocuk98 <kokocuk98@gmail.com>
2018-05-09 20:02:58 +0300

    Задание 8

---
+++ selenium-test-project/selenium-test-project/TestSticker.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Collections.ObjectModel;
+
+namespace csharp_example
+{
+    class ProductTest
+    {
+        public ChromeDriver driver;
+
+        [SetUp]
+        public void Start()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void TestSticker()
+        {
+            //Заходим и логинимся
+            driver.Url = "http://localhost/litecart/";
+            
+            int MenuCount = driver.FindElements(By.XPath("//*[@class='product column shadow hover-light']")).Count;
+            ReadOnlyCollection<IWebElement> menu = driver.FindElements(By.XPath("//*[@class='product column shadow hover-light']"));
+
+            for (int i = 0; i <= MenuCount - 1; i++)
+            {
+                menu[i].FindElement(By.XPath(".//*[contains(@class, 'sticker sale') or contains(@class, 'sticker new')]"));
+                int StickerCount = menu[i].FindElements(By.XPath("//*[contains(@class, 'sticker sale') or contains(@class, 'sticker new')]")).Count;
+                if (StickerCount > 1 || StickerCount < 1)
+                    Assert.Fail("Больше одного стикера у уточки");
+
+            }
+        }
+
+        [TearDown]
+        public void Stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
